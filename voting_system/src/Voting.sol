cbd8df8505be0a20656198f9885bdce840a36ae2
x86Cow <62828198+x86Cow@users.noreply.github.com>
2023-04-30 06:03:02 +0000

    finished code eand tests for PollInfo

--- voting_system/src/Voting.sol
+++ voting_system/src/Voting.sol
@@ -3,20 +3,21 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Voting {
     struct PollInfo {
+        address pollCreator;
         string title;
-        string desc;
+        string question;
         bool status;
     }
 
     PollInfo pollInfo;
-    function setPollInfo(string memory _title, string memory _desc, bool _status) public {
-        pollInfo = PollInfo(_title, _desc, _status);
+    function setPollInfo(address _pollCreator, string memory _title, string memory _question, bool _status) public {
+        pollInfo = PollInfo(_pollCreator, _title, _question, _status);
     }
     function getPollTitle() public view returns (string memory) {
         return(pollInfo.title);
     } 
-    function getPollDesc() public view returns (string memory) {
-        return(pollInfo.desc);
+    function getPollQuestion() public view returns (string memory) {
+        return(pollInfo.question);
     }
     function getPollStatus() public view returns (bool) {
         return(pollInfo.status);
--- voting_system/test/Voting.t.sol
+++ voting_system/test/Voting.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL3
 pragma solidity ^0.8.13;
 
 import "../lib/forge-std/src/Test.sol";
@@ -9,12 +9,13 @@ contract VotingTest is Test {
 
     function setUp() public {
         voting = new Voting();
-        voting.setPollInfo("Title", "This is a test", true);
+        voting.setPollInfo(0xD7EF90Eb0fF8D758BdFCC10ab7346Ff71FFecc6B , "Title", "This is a test", true);
     }
 
     function testPollInfo() public {
-        assertEq(voting.getPollTitle, "Title");
-        assertEq(voting.getPollDesc, "This is a test");
-        assertEq(voting.getPollStatus, true);
+        assertEq(voting.getPollTitle(), "Title");
+        assertEq(voting.getPollQuestion(), "This is a test");
+        assertEq(voting.getPollStatus(), true);
     }
+    // function testPollEdit() public {}
 }
